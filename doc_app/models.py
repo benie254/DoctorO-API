3f8e9adcb0fc68a1b18153a350f1ed2d29d141a9
benie254 <davinci.monalissa@gmail.com>
2023-07-11 12:46:59 +0300

    create contact model class

--- doc_app/models.py
+++ doc_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=500,default='')
+    email = models.EmailField(max_length=2000,default='')
+    message = models.TextField(max_length=10000,default='')
